2dfa8f77bf04bc7fb42af370bbe3aba3864933f6
Franish <160093223+Franish@users.noreply.github.com>
2024-07-16 20:08:20 +0530

    Initial commit for the project

---
+++ src/app/components/details/details.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit,OnChanges, SimpleChanges, AfterContentInit, AfterViewInit } from '@angular/core';
+
+@Component({
+  selector: 'app-details',
+  templateUrl: './details.component.html',
+  styleUrl: './details.component.css'
+})
+export class DetailsComponent {
+
+  constructor(){
+
+  }
+  
+}
